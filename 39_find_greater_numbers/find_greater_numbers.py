86798549752a887b5581b8b1cdeae04f0ac7d2a6
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-04-05 17:08:54 -0400

    completed exercises 36-39

---
+++ 39_find_greater_numbers/find_greater_numbers.py
@@ -0,0 +1,27 @@
+def find_greater_numbers(nums):
+    """Return # of times a number is followed by a greater number.
+
+    For example, for [1, 2, 3], the answer is 3:
+    - the 1 is followed by the 2 *and* the 3
+    - the 2 is followed by the 3
+
+    Examples:
+
+        >>> find_greater_numbers([1, 2, 3])
+        3
+
+        >>> find_greater_numbers([6, 1, 2, 7])
+        4
+
+        >>> find_greater_numbers([5, 4, 3, 2, 1])
+        0
+
+        >>> find_greater_numbers([])
+        0
+    """
+    counter = 0
+    for num in range(len(nums)):
+        for next in range(num +1, len(nums)):
+            if nums[next] > nums[num]:
+                counter += 1
+    return counter
