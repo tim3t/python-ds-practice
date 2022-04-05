35cf999d7c2bb5882429b31c44b43e743ecf1cb9
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-04-04 22:22:15 -0400

    completed exercises 16-20

---
+++ 17_mode/mode.py
@@ -0,0 +1,20 @@
+def mode(nums):
+    """Return most-common number in list.
+
+    For this function, there will always be a single-most-common value;
+    you do not need to worry about handling cases where more than one item
+    occurs the same number of times.
+
+        >>> mode([1, 2, 1])
+        1
+
+        >>> mode([2, 2, 3, 3, 2])
+        2
+    """
+    counter = {}
+    for num in nums:
+        counter[num] = counter.get(num, 0) + 1
+    max_count = max(counter.values())
+    for (num,freq) in counter.items():
+        if freq == max_count:
+            return num
